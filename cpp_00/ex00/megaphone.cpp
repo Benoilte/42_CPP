30110a1867b4a6a244cc80c3befddc7fca841f17
benoit brandt <benoit.07@hotmail.com>
2024-11-05 14:35:00 +0100

    Update compilation for CPP ex00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -3,10 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:39:20 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/11/04 16:39:21 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:00:50 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main()
+{
+	std::cout << "Hello from megaphone main function!" << std::endl;
+	return (0);
+}
