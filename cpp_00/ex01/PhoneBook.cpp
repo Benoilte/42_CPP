22398c300249983f4187abfe2274167a2f7901ea
Benoilte <benoit.07@hotmail.com>
2024-11-07 20:04:21 +0100

    add first cpp class, constructor and destructor

--- cpp_00/ex01/PhoneBook.cpp
+++ cpp_00/ex01/PhoneBook.cpp
@@ -6,7 +6,37 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:39:09 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/11/04 16:39:10 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/11/07 19:43:48 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void)
+{
+    this->tab = 8;
+    std::cout << "This is the constructor called" << std::endl;
+}
+
+PhoneBook::~PhoneBook(void)
+{
+    std::cout << "This is the destructor called" << std::endl;
+}
+
+void    PhoneBook::add(void)
+{
+    std::cout << "This is the add method" << std::endl;
+    std::cout << "Tab: " << this->tab << std::endl;
+}
+
+void    PhoneBook::search(void)
+{
+    std::cout << "This is the search method" << std::endl;
+    std::cout << "Tab: " << this->tab << std::endl;
+}
+
+void    PhoneBook::exit(void)
+{
+    std::cout << "This is the exit method" << std::endl;  
+}
--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 16:39:06 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/11/07 19:42:12 by bebrandt         ###   ########.fr       */
+/*   Created: 2024/11/07 19:21:46 by bebrandt          #+#    #+#             */
+/*   Updated: 2024/11/07 19:44:07 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
+#include "PhoneBook.hpp"
 
-class PhoneBook
+int main(void)
 {
-    public:
+    PhoneBook   directory;
 
-    int tab;
+    directory.add();
+    directory.tab = 42;
+    directory.search();
+    directory.exit();
 
-    PhoneBook(void);
-    ~PhoneBook(void);
-
-    void    add(void);
-    void    search(void);
-    void    exit(void);
-};
-
-#endif
+    return (0);
+}
