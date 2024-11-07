9448e08077530d2894e76ed3553580fdf57f6630
benoit brandt <benoit.07@hotmail.com>
2024-11-07 10:34:24 +0100

    add logic for megaphone exercise

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,14 +6,31 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:39:20 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/11/05 13:00:50 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/11/07 09:03:15 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-int	main()
+void	loud_and_unbearable_noise()
 {
-	std::cout << "Hello from megaphone main function!" << std::endl;
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
+
+void	megaphone(std::string const& str)
+{
+	std::cout << str << std::endl;
+}
+
+int	main(int argc, char* argv[])
+{
+	if (argc < 2)
+		loud_and_unbearable_noise();
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			megaphone(argv[i]);
+	}
 	return (0);
 }
