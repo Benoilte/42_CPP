449a79b30df1c161ea85ba35d04c1113e7e2b319
Benoilte <benoit.07@hotmail.com>
2024-11-22 15:34:58 +0100

    add main prompt for Phone Book

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:21:46 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/11/22 14:40:58 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/11/22 15:33:40 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,23 @@
 
 int main(void)
 {
-    PhoneBook   directory;
+	PhoneBook   directory;
+	std::string input;
 
-    std::cout << "Do some stuff with your PhoneBook" << std::endl;
-    
-    return (0);
+	std::cout << "Welcome in your Phone Book.\nEnter 'ADD' - 'SEARCH' - 'EXIT' " << std::endl;
+	while((input.compare("EXIT")) != 0)
+	{
+		std::getline(std::cin >> std::ws, input);
+		if ((input.compare("ADD")) == 0)
+		{
+			directory.add();
+			std::cout << "Enter 'ADD' - 'SEARCH' - 'EXIT' " << std::endl;
+		}
+		if ((input.compare("SEARCH")) == 0)
+		{
+			directory.search();
+			std::cout << "Enter 'ADD' - 'SEARCH' - 'EXIT' " << std::endl;
+		}
+	}
+	return (0);
 }
