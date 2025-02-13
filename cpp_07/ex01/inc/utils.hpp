1da6bbeb95106ae14fc44008d46284369a3aa195
Benoilte <benoit.07@hotmail.com>
2025-02-13 14:51:28 +0100

    add template iter and iter in read only mode && Add explicit test

--- cpp_07/ex01/inc/utils.hpp
+++ cpp_07/ex01/inc/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:05:07 by bebrandt          #+#    #+#             */
-/*   Updated: 2025/02/13 13:03:25 by bebrandt         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:27:31 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 
 #include "iter.hpp"
+#include <cstddef>
 #include "colors.hpp"
 
 #endif
--- cpp_07/ex01/src/main.cpp
+++ cpp_07/ex01/src/main.cpp
@@ -1,11 +1,101 @@
-#include "iter.hpp"
+#include "utils.hpp"
 #include <iostream>
 
-int	main(int argc, char **argv)
+void	addTwo(int &n)
 {
-	(void)argc;
-	(void)argv;
-	std::cout << "Hello World" << std::endl;
+	n += 2;
+}
+
+void	displayInt(int const &n)
+{
+	std::cout << n << std::endl;
+}
+
+void	defineLetter(char const &c)
+{
+	std::string	vowels = "aeiouyAEIOUY";
+
+	if ((vowels.find(c, 0) != std::string::npos))
+		std::cout << c << ": vowel" << std::endl;
+	else if (std::isalpha(c))
+		std::cout << c << ": consonant" << std::endl;
+}
+
+void	RotateLowercase(char &c)
+{
+	char min = 'a', max = 'z';
+
+	if (std::islower(c))
+		c = max - (c - min);
+}
+
+void	testIntArray()
+{
+	int	intArray[3] = {2, 4, 6};
+	size_t	length  = 3;
+
+	std::cout << CYAN << "Test with array of int and function to add 2" << RESET << std::endl;
+	for (size_t i = 0; i < length; i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
+	iter(intArray, length, addTwo);
+	std::cout << YELLOW << "Function iter is called" << RESET << std::endl;
+	for (size_t i = 0; i < length; i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;	
+}
+
+void	testConstIntArray()
+{
+	int	const intArray[3] = {2, 4, 6};
+	size_t	length  = 3;
+
+	std::cout << CYAN << "Test with array of const int and function to display element" << RESET << std::endl;
+	for (size_t i = 0; i < length; i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
+	std::cout << YELLOW << "Function iter is called" << RESET << std::endl;
+	iter(intArray, length, displayInt);
+}
+
+void	testConstAlphabet()
+{
+	char const	intArray[27] = {'a', 'b', 'c', 'd', 'E', 'F', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ';'};
+	size_t	length  = 27;
+
+	std::cout << CYAN << "Test with array of const char and function to display if letter is consonant or vowel" << RESET << std::endl;
+	for (size_t i = 0; i < length; i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
+	std::cout << YELLOW << "Function iter is called" << RESET << std::endl;
+	iter(intArray, length, defineLetter);
+}
+
+void	testLowercaseRotate()
+{
+	char	intArray[8] = {'a', 'b', 'c', 'A', ';', 'x', 'y', 'z'};
+	size_t	length  = 8;
+
+	std::cout << CYAN << "Test with array of char and function to rotate lower case letter" << RESET << std::endl;
+	for (size_t i = 0; i < length; i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
+	std::cout << YELLOW << "Function iter is called" << RESET << std::endl;
+	iter(intArray, length, RotateLowercase);
+	for (size_t i = 0; i < length; i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
+}
+
+int	main(void)
+{
+	testIntArray();
+	std::cout << std::endl;
+	testConstIntArray();
+	std::cout << std::endl;
+	testConstAlphabet();
+	std::cout << std::endl;
+	testLowercaseRotate();
 	return (0);
 }
 
