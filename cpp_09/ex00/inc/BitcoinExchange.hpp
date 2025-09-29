cf43cc6ef927788dbaedd65af1683d60316ad214
Benoilte <benoit.07@hotmail.com>
2025-09-29 15:25:42 +0200

    Parse date\

--- cpp_09/ex00/inc/BitcoinExchange.hpp
+++ cpp_09/ex00/inc/BitcoinExchange.hpp
@@ -6,6 +6,7 @@
 # include <sstream>
 # include <string>
 # include <ctime>
+# include <cstdlib>
 # include <exception>
 
 class BitcoinExchange
@@ -28,7 +29,7 @@ class BitcoinExchange
 
 			public:
 				BtcException() throw();
-				BtcException(const char* msg) throw();
+				BtcException(std::string msg) throw();
 				virtual ~BtcException() throw();
 				virtual const char	*what() const throw();
 		};
@@ -45,7 +46,12 @@ class BitcoinExchange
 
 		void	initDataBase();
 		void	parseDate(std::string &line, std::string &date);
+		void	validYear(const std::string &date, const int &year);
+		void	validMonth(const std::string &date, const int &month);
+		void	validDay(const std::string &date, const int &year, const int &month, const int &day);
+		bool	isLeapYear(const int &year);
 		void	parseExchangeRate(std::string &line, float &exchangeRate);
+		const std::string	convertToString(const int &n);
 };
 
 std::ostream	&operator<<(std::ostream &out, BitcoinExchange const &rhs);
--- cpp_09/ex00/src/BitcoinExchange.cpp
+++ cpp_09/ex00/src/BitcoinExchange.cpp
@@ -62,7 +62,7 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange &rhs)
 
 void	BitcoinExchange::init(void)
 {
-	m_dataBaseFile.open("data.csv");
+	m_dataBaseFile.open("errorData.csv");
 	if (!m_dataBaseFile.is_open())
 		throw BtcException("failed to open data base file");
 
@@ -81,20 +81,101 @@ void	BitcoinExchange::initDataBase(void)
 
 	std::getline(m_dataBaseFile, line);
 	if (line != "date,exchange_rate")
-		throw BtcException("Database title does not match: date,exchange_rate");
+		throw BtcException("Database title invalid format, Expected: date,exchange_rate");
 	std::getline(m_dataBaseFile, line);
-	while (!m_dataBaseFile.eof())
+	while (!line.empty() || !m_dataBaseFile.eof())
 	{
-		std::cout << line.substr(0, line.find(',')) << std::endl;
-		parseDate(line, date);
-		parseExchangeRate(line, exchangeRate);
+		try
+		{
+			parseDate(line, date);
+			parseExchangeRate(line, exchangeRate);
+			std::cout << line.substr(0, line.find(',')) << std::endl;
+			line.clear();
+		}
+		catch(const BitcoinExchange::BtcException& e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
 		std::getline(m_dataBaseFile, line);
 	}
 }
 
+// 2009-02-07
 void	BitcoinExchange::parseDate(std::string &line, std::string &date)
 {
 	date = line.substr(0, line.find(','));
+
+	if (date.size() != 10)
+		throw BtcException("Date size invalid, Expected: yyyy-mm-dd");
+	for (int i = 0; i < 10; i++)
+	{
+		if ((i == 4) || (i == 7))
+		{
+			if (date[i] != '-')
+				throw BtcException("Invalid separator (" + date + "). Expected: yyyy-mm-dd");
+		}
+		else if (!std::isdigit(date[i]))
+			throw BtcException("Invalid date format (" + date + "). Date should contain only digit as expected: yyyy-mm-dd");
+	}
+
+	int	year = std::atoi(date.substr(0, 4).c_str());
+	int	month = std::atoi(date.substr(5, 2).c_str());
+	int	day = std::atoi(date.substr(8, 2).c_str());
+
+	validYear(date, year);
+	validMonth(date, month);
+	validDay(date, year, month, day);
+
+	if (date > m_todayDate)
+		throw BtcException("Invalid date format (" + date + ") the provided date is later than today. Expected a date earlier or equal than today: " + m_todayDate);
+}
+
+void	BitcoinExchange::validYear(const std::string &date, const int &year)
+{
+	std::string invalidYearMsg = "Invalid year format (" + date + "). Expected between: 2000 - " + m_todayDate.substr(0, 4);
+
+	if (year < 2000 || year > std::atoi(m_todayDate.substr(0, 4).c_str()))
+		throw BtcException(invalidYearMsg);
+}
+
+void	BitcoinExchange::validMonth(const std::string &date, const int &month)
+{
+	if ((month < 1) || (month > 12))
+		throw BtcException("Invalid month format (" + date + "). Expected between: 01 - 12");
+}
+
+void	BitcoinExchange::validDay(const std::string &date, const int &year, const int &month, const int &day)
+{
+	int maxDay = 31;
+
+	if (month == 2)
+	{
+		if (isLeapYear(year))
+			maxDay = 29;
+		else
+			maxDay = 28;  
+	}
+	else if ((month == 4) || (month == 6) || (month == 9) || (month == 11))
+		maxDay = 30;
+
+	if ((day < 1) || (day > maxDay))
+		throw BtcException("Invalid day format (" + date + "). Expected between: 01 - " + convertToString(maxDay));
+}
+
+bool	BitcoinExchange::isLeapYear(const int &year)
+{
+	if ((year % 400) == 0)
+			return true;
+	else if ((year % 100) == 0)
+			return false;
+	else if ((year % 4) == 0)
+			return true;
+	else
+			return false;
 }
 
 void	BitcoinExchange::parseExchangeRate(std::string &line, float &exchangeRate)
@@ -103,7 +184,16 @@ void	BitcoinExchange::parseExchangeRate(std::string &line, float &exchangeRate)
 
 	ss << line.substr(line.find(',') + 1);
 	ss >> exchangeRate;
-	if (!ss.good())
+	if (ss.fail())
+		throw BtcException("Exchange rate invalid format");
+}
+
+const std::string		BitcoinExchange::convertToString(const int &n)
+{
+	std::stringstream ss;
+
+	ss << n;
+	return ss.str();
 }
 
 void	BitcoinExchange::display(void)
@@ -119,7 +209,7 @@ BitcoinExchange::BtcException::BtcException() throw()
 	// Custom Exception default Constructor
 }
 
-BitcoinExchange::BtcException::BtcException(const char* msg) throw()
+BitcoinExchange::BtcException::BtcException(std::string msg) throw()
 : m_message(msg)
 {
 	// Custom Exception Constructor
